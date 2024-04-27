1fc05849b7621c109b6fc9bc6ec9e8e8da54ff32
Andrew Fowlie <andrew.fowlie@xjtlu.edu.cn>
2024-04-28 00:22:05 +0800

    hold hugo version

--- install_hugo.sh
+++ install_hugo.sh
@@ -1,3 +1,4 @@
 wget https://github.com/gohugoio/hugo/releases/download/v0.59.0/hugo_0.59.0_Linux-64bit.deb
 dpkg -i hugo_0.59.0_Linux-64bit.deb
 rm hugo_0.59.0_Linux-64bit.deb
+echo "hugo hold" | sudo dpkg --set-selections
